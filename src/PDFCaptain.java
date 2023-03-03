af118521fe07de77c30399b7974641494080c3ba
EDragoev <edragoev@protonmail.com>
2023-03-02 23:11:36 -0500

    Added more code to make the table look better.

--- src/PDFCaptain.java
+++ src/PDFCaptain.java
@@ -11,7 +11,6 @@ import java.net.Socket;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static org.eclipse.swt.SWT.KeyDown;
 
@@ -541,7 +540,18 @@ class FileInfo {
             } else if (tokens[0].equals("Pages")) {
                 numberOfPages = tokens[1].trim();
             } else if (tokens[0].equals("Page size")) {
-                pageSize = tokens[1].trim();
+                String[] xy = tokens[1].split(" x ");
+                if (xy[0].contains(".")) {
+                    pageSize = xy[0].substring(0, xy[0].indexOf("."));
+                } else {
+                    pageSize = xy[0];
+                }
+                pageSize += " x ";
+                if (xy[1].contains(".")) {
+                    pageSize += xy[1].substring(0, xy[1].indexOf(".")) + xy[1].substring(xy[1].indexOf(" pts"));
+                } else {
+                    pageSize += xy[1];
+                }
             }
         }
     }
